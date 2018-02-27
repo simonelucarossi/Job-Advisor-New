a7644db8578ea254c37cc6074d1af5db951c0bd5
simonerossi <simonerossi@protonmail.com>
2018-02-27 14:03:16 +0100

    Change db

--- src/com/jobadvisor/persistence/PostgresDAOFactory.java
+++ src/com/jobadvisor/persistence/PostgresDAOFactory.java
@@ -13,7 +13,7 @@ class PostgresDAOFactory extends DAOFactory {
 			Class.forName("org.postgresql.Driver").newInstance();
 			// DataSource("jdbc:postgresql://52.39.164.176:5432/xx","xx","p@xx");
 			// dataSource = new DataSource("jdbc:postgresql://localhost:5432/JobAdvisor", "postgres", "postgres");
-			dataSource = new DataSource("jdbc:postgresql://baasu.db.elephantsql.com:5432/njwtaomy", "njwtaomy", "9jMfdBOALh13bXoUvE7mS90lwFexIMf0");
+			dataSource = new DataSource("jdbc:postgresql://horton.elephantsql.com:5432/aqdvooyg", "aqdvooyg", "Jy42xKF8E0FYImE397QIcRvRovRTaiVZ");
 		} catch (Exception e) {
 			System.err.println("PostgresDAOFactory.class: failed to load MySQL JDBC driver\n" + e);
 			e.printStackTrace();
